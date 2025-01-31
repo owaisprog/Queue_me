c181fba5c4916822621d1cbdd3b68709cd98b8e5
Owais Dev <140313737+owaisprog@users.noreply.github.com>
2025-01-31 09:54:29 +0500

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,45 +1,8 @@
-// /**
-//  * @type {import('next').NextConfig}
-//  */
-
-// const isStaticExport = 'false';
-
-// const nextConfig = {
-//   trailingSlash: true,
-//   env: {
-//     BUILD_STATIC_EXPORT: isStaticExport,
-//   },
-//   modularizeImports: {
-//     '@mui/icons-material': {
-//       transform: '@mui/icons-material/{{member}}',
-//     },
-//     '@mui/material': {
-//       transform: '@mui/material/{{member}}',
-//     },
-//     '@mui/lab': {
-//       transform: '@mui/lab/{{member}}',
-//     },
-//   },
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ['@svgr/webpack'],
-//     });
-
-//     return config;
-//   },
-//   ...(isStaticExport === 'true' && {
-//     output: 'export',
-//   }),
-// };
-
-// export default nextConfig;
-
 /**
  * @type {import('next').NextConfig}
  */
 
-const isStaticExport = false;
+const isStaticExport = 'false';
 
 const nextConfig = {
   trailingSlash: true,
@@ -65,7 +28,7 @@ const nextConfig = {
 
     return config;
   },
-  ...(isStaticExport === true && {
+  ...(isStaticExport === 'true' && {
     output: 'export',
   }),
 };
