12831d494824b032bfcf8c154e966917003e04ae
Owais Dev <140313737+owaisprog@users.noreply.github.com>
2025-01-31 09:10:23 +0500

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,52 @@
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+
+// const isStaticExport = 'false';
+
+// const nextConfig = {
+//   trailingSlash: true,
+//   env: {
+//     BUILD_STATIC_EXPORT: isStaticExport,
+//   },
+//   modularizeImports: {
+//     '@mui/icons-material': {
+//       transform: '@mui/icons-material/{{member}}',
+//     },
+//     '@mui/material': {
+//       transform: '@mui/material/{{member}}',
+//     },
+//     '@mui/lab': {
+//       transform: '@mui/lab/{{member}}',
+//     },
+//   },
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ['@svgr/webpack'],
+//     });
+
+//     return config;
+//   },
+//   ...(isStaticExport === 'true' && {
+//     output: 'export',
+//   }),
+// };
+
+// export default nextConfig;
+
+
 /**
  * @type {import('next').NextConfig}
  */
 
-const isStaticExport = 'false';
+// Use an environment variable to determine if static export is enabled
+const isStaticExport = process.env.BUILD_STATIC_EXPORT === 'true';
 
 const nextConfig = {
   trailingSlash: true,
   env: {
-    BUILD_STATIC_EXPORT: isStaticExport,
+    BUILD_STATIC_EXPORT: isStaticExport ? 'true' : 'false',
   },
   modularizeImports: {
     '@mui/icons-material': {
@@ -28,8 +67,12 @@ const nextConfig = {
 
     return config;
   },
-  ...(isStaticExport === 'true' && {
+  // Enable static export if BUILD_STATIC_EXPORT is true
+  ...(isStaticExport && {
     output: 'export',
+    images: {
+      unoptimized: true, // Disable image optimization for static export
+    },
   }),
 };
 
